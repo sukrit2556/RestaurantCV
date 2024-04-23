647fa26c25db88e87f1229297453ff6b1e070ba3
sukrit2556 <sukrit2556@gmail.com>
2024-04-24 06:27:44 +0700

    commit of 6.27AM delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-import pygetwindow as gw
-import pyautogui
-import cv2
-import numpy as np
-
-# Get a list of all windows
-windows = gw.getAllWindows()
-for index, window in enumerate(windows):
-    print(index, window.title)
